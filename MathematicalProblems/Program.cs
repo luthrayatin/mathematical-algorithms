10e99a5c116a50aab1155c48dc762e69e814f9b7
Yatin Luthra <luthrayatin@gmail.com>
2017-02-21 10:00:13 +1000

    Initial commit

---
+++ MathematicalProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace MathematicalProblems
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+        }
+    }
+}
